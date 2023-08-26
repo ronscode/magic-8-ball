595180d78856f3b81c036d5fd22baf35ea4d43f1
Ron Cagle <ron@roncagle.com>
2023-08-26 12:50:58 -0400

    trying something

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+async () => {
+            try {
+                const response = await fetch('/get-secret-data/.env');
+                const data = await response.json();
+                console.log(data)
+            } catch (error) {
+                document.getElementById('output').textContent = 'Error fetching data.';
+            }
+        }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
